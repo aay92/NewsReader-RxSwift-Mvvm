c84d06905b0d7dbb8586786fbd136a26256265bd
Aleksey <aay92@yandex.ru>
2023-08-13 18:03:18 +0400

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Aleksey Alyonin on 13.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
